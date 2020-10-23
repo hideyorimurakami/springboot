39caf8022e32696d2fc893261192ac0323c4bcc9
hideyorimurakami <71474250+hideyorimurakami@users.noreply.github.com>
2020-10-23 13:27:01 +0900

    Add files via upload

--- SpringBoot_test/src/main/java/com/example/demo/controllers/HelloController.java
+++ SpringBoot_test/src/main/java/com/example/demo/controllers/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -11,5 +12,11 @@ public class HelloController {
 	@ResponseBody
 	public String index() {
 		return "こんにちは、世界！";
+	}
+
+	@GetMapping("/greet")
+	public String greet(Model model) {
+		model.addAttribute("message","こんにちは、世界！");
+		return "greet";
 	}
 }
